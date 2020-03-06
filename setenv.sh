77f8e39655714294936f560386cb8382df6a8f30
gaochaung <61182292+gaochaung@users.noreply.github.com>
2020-03-06 11:04:52 +0000

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -3,4 +3,4 @@
 export CATALINA_OPTS=" -XX:MaxPermSize=256m -Dakka.daemonic=on "
  
 # Set Tomcat options to prevent resource problems with larger ontology v2.0.1
-CATALINA_OPTS="$CATALINA_OPTS -server -Xms1024m -Xmx2048m -Duser.timezone=America/New_York
+CATALINA_OPTS="$CATALINA_OPTS -server -Xms1024m -Xmx2048m -Duser.timezone=America/New_York"
